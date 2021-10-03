2765dd5aa2a88b962fe7599099ba393761e7fcb2
Vince <starryblue7@gmail.com>
2021-10-03 12:36:32 -0700

    add 5-day forecast display funtionality

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-function getWeather(city, lat, lon, units) {
+function getWeather(lat, lon, city, units) {
     if (!units) {
         units = "imperial"
     }
@@ -12,36 +12,60 @@ function getWeather(city, lat, lon, units) {
         method: 'GET',
       }).then(function (response) {
         console.log(response);
-        displayToday(city, response, units);
+        displayToday(response, city, units);
         displayForecast(response, units)
       });
 }
 
-getWeather(37.795505,-122.400469);
+getWeather(37.795505,-122.400469, "San Francisco", "metric");
 
-function displayToday(city, response, units) {
+function displayToday(response, city, units) {
     const current = response.current;
     const date = moment().format("dddd, MMMM Do");
 
-    let tempUnit;
-    let speedUnit;
-    if (!units || units === "imperial") {
-        tempUnit = "F";
-        speedUnit = "m/s";
-    } else {
-        tempUnit = "C";
-        speedUnit = "mph";
-    }
-
-    const header = $('<h2>').text(city + ' on ' + date);
-    const temperature = $('<p>').text('Temperature: ' + current.temp + '°' + tempUnit);
+    const icon = getIcon(current.weather[0].icon, current.weather[0].description)
+    const header = $('<h2>').text(city + ' on ' + date).prepend(icon);
+    const temperature = $('<p>').text('Temperature: ' + current.temp + ' ' + getUnits(units).temperature);
     const humidity = $('<p>').text('Humidity: ' + current.humidity + '%');
-    const wind = $('<p>').text('Wind Speed: ' + current.wind_speed + ' ' + speedUnit);
+    const wind = $('<p>').text('Wind Speed: ' + current.wind_speed + ' ' + getUnits(units).speed);
     const uvIndexIcon = $('<p>').text('UV Index: ' + current.uvi);
 
     $('#current').html('')
     $('#current').append(header, temperature, humidity, wind, uvIndexIcon);
 }
+
+function displayForecast(response, units) {
+    const daily = response.daily;
+    
+    $('#forecast').html('')
+    for (i = 0; i < 5; i++) {
+    // daily.each(function() {
+        const date = $('<h5>').text(moment.unix(daily[i].dt).format("MM/DD"));
+        const icon = getIcon(daily[i].weather[0].icon, daily[i].weather[0].description)
+        const temperature = $('<p>').text('Temp: ' + daily[i].temp.day + ' ' + getUnits(units).temperature);
+        const humidity = $('<p>').text('Humidity: ' + daily[i].humidity + ' ' + "%");
+
+        let card = $('<div>').append(date, icon, temperature, humidity);
+        card.attr('class', 'card text-white bg-primary mr-3 mb-3 col-lg-2 col-md-3 col-sm-4');
+        $('#forecast').append(card);
+    }
+
+}
+
+function getUnits(units) {
+    if (!units || units === "imperial") {
+        return {
+            temperature: "°F",
+            speed: "mph"
+        }
+    } 
+    return {
+        temperature: "°C",
+        speed: "m/s"
+    }
+}
+
+
 
 function getIcon(iconCode, description) {
     return "<img alt='" + description + " icon' src='http://openweathermap.org/img/wn/" + iconCode + "@2x.png'>"
